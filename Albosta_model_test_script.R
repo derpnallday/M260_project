95fb0214f6a6986df7a398b707bfdfaeb35c4a08
derpnallday <derek.albosta@gmail.com>
2018-12-04 01:44:08 -0800

    r documentation

--- Albosta_model_test_script.R
+++ Albosta_model_test_script.R
@@ -1,17 +1,17 @@
 ###################################################
 # Derek Albosta
-# Date: 2013.04.11
+# Date: 2018.04.12
 #
 ###################################################
 # Model selection
 
-#MPG for cars given its specifications
+#model to predict MPG for cars given its specifications
 
 # set up
-require (car)   # for normal quantile plots
+require (car) # for normal quantile plots
 require(DAAG) # for vif
 
-# Set up for plotting. We will be using two groups (two professions)
+# Set up for plotting. We will be using two groups (engine types and transmission types)
 # so the following lines set up color codes and plotting symbols
 # for them
 colors = c("red","blue") # colors for our plots
@@ -45,18 +45,10 @@ engineTypes<-c("V-shaped","S-shaped")
 boxplot(cars$mpg,cars$vs, names=engineTypes, ylab="MPG") 
 tranType<-c("Auto","Manual")
 boxplot(cars$mpg,cars$am, names=tranType, ylab="MPG")
+#they look exactly the same, impling they are highly correlated, so throwing out am var for vs
 
-
-# OK, explore a little. Draw a picture
+# scatterplot matrix
 pairs(cars)
-
-#looks like we want to remove dips, cyl, and drat
-#since they are highly correlated with other vars
-#also removing gear since it has lowest correlation
-
-c = subset(cars, select = c(mpg,hp,wt,vs,am))
-pairs (c)
-
 
 #subset for cor matrix of all remaning numeric vars
 k = subset(cars, select = c(mpg,cyl,disp,hp,wt,qsec,gear,carb))
@@ -64,12 +56,19 @@ k = subset(cars, select = c(mpg,cyl,disp,hp,wt,qsec,gear,carb))
 # Construct a correlation matrix now as well.
 cor(k,use="complete.obs")
 
+#looks like we want to remove disp, cyl, and drat
+#since they are highly correlated with other vars
+#also removing gear since it has lowest correlation
+c = subset(cars, select = c(mpg,hp,wt,vs))
+pairs (c)
+
 
 ###########################################################
 #
 # Model Testing
 #
 ###########################################################
+#MODEL 1
 # see how well they produce a model
 #on mpg~weight since weight was best correlation with our response
 m1 = lm(mpg~wt, data = cars, na.action = na.exclude)
@@ -85,8 +84,7 @@ qqPlot(resid(m1))
 plot(resid(m1)~wt, data = cars)
 abline(h=0)
 
-
-
+#MODEL 2
 #lets try combining vars 
 #just numerics
 m2 = lm(mpg~wt+hp, data = cars, na.action = na.exclude)
@@ -104,9 +102,7 @@ abline(h=0)
 plot(resid(m2)~hp, data = cars)
 abline(h=0)
 
-
-
-
+#MODEL 3
 #with vs and both numerics
 m3 = lm(mpg~vs+wt+hp, data = cars, na.action = na.exclude)
 summary(m3)
@@ -123,8 +119,7 @@ abline(h=0)
 plot(resid(m3)~hp, data = cars)
 abline(h=0)
 
-
-
+#MODEL 4
 #now try with interaction terms
 #try hp*wt with vs since it makes sense for weight and hp to have an interaction on MPG
 m4 = lm(mpg~vs+wt*hp, data = cars, na.action = na.exclude)
@@ -143,13 +138,10 @@ plot(resid(m2)~hp, data = cars)
 abline(h=0)
 
 
-
-
 #test models
 AIC(m1,m2,m3,m4)
 
-#best model is m9
-
+#best model is m4
 m = m4
 summary(m)
 
@@ -210,32 +202,15 @@ plot(mpg~hp, col=colors[as.numeric(cars$vs)],
 abline(beta0,betaHp, col=colors[1])      
 abline(beta0 + betaVs,betaHp, col=colors[2])
 
-# it's nice to add a legend (see ? legend for more info)
+# add legend
 legend("topright", fill=colors, c("V-shaped","S-Shaped"))  
      
 
-# Test result:
-           # Best model takes both HP and Weight with engine type as the interaction term
-           
-
-# Interpretation:
-
-#  Mean systolic blood carsure seems to be associated with age and occupation (F-test)
-#  Both age and occupation seem to be associated with sbp even if the other explanatory variable
-#  is in the model (t-tests of coefficients)
-#  This model explains about 90% of the variation in sbp (multiple R^2)
-#  Mean sbp increases an estimated 1.54 points for each one-year increase in age (Beta1^)
-#  Mean sbp is lower by an estimated 7.51 points for university lecturers (vs.journalists) (Beta0^)
-confint(m)
+###################################
 #
-#  True mean sbp increase for each one-year increase in age is estimated to be 
-#  in the interval (1.32, 1.76) ( 95% CI for Beta1)
-#  True difference in mean sbp for university lecturers is estimated to be 4.19 to 10.84 units
-#  lower than mean sbp for journalists (95% CI for Beta0)
-
-# Use the model
-
-# estimate mean for a particular value of age and occupation
+# Using model
+#
+###################################
 
 # V-shaped engine with 110 hp and 2.62 (thousand) as weight
 #actual is 21 mpg - prediction 22.39
@@ -287,19 +262,3 @@ predict(m, new = car4, interval = "prediction" )
 # since this data is using older vehicles and technolgy has change so much since the test date.
 
 ########################################################
-#Now lets do some more analysis on model
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

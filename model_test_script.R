3ffab9280bb77d293206330f4fdf9a340700aaa8
derpnallday <derek.albosta@gmail.com>
2018-12-03 22:52:38 -0800

    finished model testing in word document

--- model_test_script.R
+++ model_test_script.R
@@ -1,43 +1,6 @@
 ###################################################
-#
-# Blood carsure study
-# This data is taken from a study of blood carsures from 
-# 28 adult males. The following comes from the description of the study:
-#
-# "It is well-known that blood carsure increases with age.
-# In this dataset we examine this relation. Age and systolic 
-# blood carsure where measured for 28 males. 15 of these are 
-# university teachers, while the remaining 13 are journalists. 
-# Along with the interest in the overall increase in systolic 
-# blood carsure, we can compare the regression lines between 
-# the two groups. [Note: data are not genuine.] "
-
-# source: http://statmaster.sdu.dk/courses/st111/data/index.html#SECTION00013700000000000000
-
-# Original Author: Chuck Hommel
+# Derek Albosta
 # Date: 2013.04.11
-#
-# Revised by:  Wendy Dove
-# Date:  2015.04.20
-#
-# Our goal is to model systolic blood carsure using the age
-# and profession of the subject as predictors. 
-# This script is intended to provide a basic model for model
-# selection.
-# 
-# The basic outline is this:
-#     1. hypothesize a model - here we think that systolic
-#        blood carsure might be modeled by the age and profession
-#        of the subjects
-#     2. Collect the data - the web site has the data for us - but in
-#        real life, this is hard and requires a design
-#     3. Build the model
-#     4. Check that the model is reasonable - are SVAs reasonable?
-#     5. Assess the model - any indications that we can do better 
-#        by using a different set of explanatory variables? transformations?
-#        etc.?
-#     6. Interpret the model - what does it say?
-#     7. Use the model for estimation/prediction
 #
 ###################################################
 # Model selection
@@ -59,17 +22,33 @@ cars = read.csv(file.choose())
 head(cars)
 names(cars)
 
-#take a look at our respone var
-hist(cars$mpg)
-summary(cars$mpg)
-
 #convert respective vars to catagorical vars
 cars$vs = factor(cars$vs)
 cars$am = factor(cars$am)
 
+#take a look at our vars
+par(mfrow=c(1,1))
+hist(cars$mpg, main="Histogram of MPG", ylab="MPG")
+summary(cars$mpg)
+summary(cars$cyl)
+summary(cars$disp)
+summary(cars$hp)
+summary(cars$drat)
+summary(cars$wt)
+summary(cars$qsec)
+summary(cars$gear)
+summary(cars$carb)
+
+#boxplots for catagorical vars
+par(mfrow=c(1,2))
+engineTypes<-c("V-shaped","S-shaped")
+boxplot(cars$mpg,cars$vs, names=engineTypes, ylab="MPG") 
+tranType<-c("Auto","Manual")
+boxplot(cars$mpg,cars$am, names=tranType, ylab="MPG")
+
+
 # OK, explore a little. Draw a picture
 pairs(cars)
-
 
 #looks like we want to remove dips, cyl, and drat
 #since they are highly correlated with other vars
@@ -86,18 +65,17 @@ k = subset(cars, select = c(mpg,cyl,disp,hp,wt,qsec,gear,carb))
 cor(k,use="complete.obs")
 
 
-
-#######################################################
+###########################################################
 #
-# MODEL TESTING
+# Model Testing
 #
-#######################################################
-# build individual models on numeric explanatory vars to
+###########################################################
 # see how well they produce a model
-#on mpg~weight
+#on mpg~weight since weight was best correlation with our response
 m1 = lm(mpg~wt, data = cars, na.action = na.exclude)
 summary(m1)
 
+par(mfrow=c(1,2))
 # SVAs
 #qq plot slight departure at upper quantile
 qqPlot(resid(m1))
@@ -106,98 +84,73 @@ qqPlot(resid(m1))
 plot(resid(m1)~wt, data = cars)
 abline(h=0)
 
-#mpg~wt shows good negative correlation
-plot(mpg~wt, data = cars)
-abline(m1)
+AIC(m1)
 
-#test mpg~hp
-m2 = lm(mpg~hp, data = cars, na.action = na.exclude)
+
+#lets try combining vars 
+#just numerics
+m2 = lm(mpg~wt+hp, data = cars, na.action = na.exclude)
 summary(m2)
 
+par(mfrow=c(1,3))
 # SVAs
 #qq plot slight departure at upper quantile
 qqPlot(resid(m2))
 
-#variance more even in middle
+#variances in center is good but spreads on upper and lower range for both
+par(mfrow=c(1,2))
+plot(resid(m2)~wt, data = cars)
+abline(h=0)
 plot(resid(m2)~hp, data = cars)
 abline(h=0)
 
-#mpg~wt shows decent negative correlation
-plot(mpg~hp, data = cars)
-abline(m2)
+AIC(m2)
 
-
-##########################################################
-#build models on catagorical vars
-#test mpg~vs
-m3 = lm(mpg~vs, data = cars, na.action = na.exclude)
-summary(m3)
-
-# SVAs
-#qq looks good
-qqPlot(resid(m3))
-
-#variances are skewed slightly to right for both types
-plot(resid(m3)~vs, data = cars)
-
-
-#mpg~am
-m4 = lm(mpg~am, data = cars, na.action = na.exclude)
-summary(m4)
-
-# SVAs
-#qq looks good
-qqPlot(resid(m4))
-
-#variances are skewed slightly to right for manual
-plot(resid(m4)~am, data = cars)
-
-###########################################################
-#lets try combining vars and do some AIC testing
-#just numerics
-m5 = lm(mpg~wt+hp, data = cars, na.action = na.exclude)
-summary(m5)
-#AIC = 156.6523
 
 #with vs and both numerics
-m6 = lm(mpg~vs+wt+hp, data = cars, na.action = na.exclude)
-summary(m6)
-#AIC = 157.5052
+m3 = lm(mpg~vs+wt+hp, data = cars, na.action = na.exclude)
+summary(m3)
 
-#with am and both numerics
-m7 = lm(mpg~am+wt+hp, data = cars, na.action = na.exclude)
-summary(m7)
-#AIC = 156.1348
+par(mfrow=c(1,3))
+# SVAs
+#qq plot slight departure at upper quantile
+qqPlot(resid(m3))
 
-#using all selected vars
-m8 = lm(mpg~am+vs+wt+hp, data = cars, na.action = na.exclude)
-summary(m8)
-#AIC = 156.0584
+#variance for hp slight improvement from last model
+plot(resid(m3)~wt, data = cars)
+abline(h=0)
+plot(resid(m3)~hp, data = cars)
+abline(h=0)
 
-##########################################################
+
+AIC(m3)
+
 #now try with interaction terms
+#try hp*wt with vs since it makes sense for weight and hp to have an interaction on MPG
+m4 = lm(mpg~vs+wt*hp, data = cars, na.action = na.exclude)
 
-#we already know from AIC model with both hp and wt are better
-#with hp*wt with am
-m9 = lm(mpg~am+wt*hp, data = cars, na.action = na.exclude)
-summary(m9)
-#AIC = 150.4317
+par(mfrow=c(1,3))
+# SVAs
+#qq plot slight departure at upper quantile
+qqPlot(resid(m4))
 
-#try hp*wt with vs
-m10 = lm(mpg~vs+wt*hp, data = cars, na.action = na.exclude)
-summary(m10)
+#variance for hp slight improvement from last model
+par(mfrow=c(1,3))
+plot(resid(m4)~wt, data = cars)
+abline(h=0)
+plot(resid(m2)~hp, data = cars)
+abline(h=0)
+summary(m4)
 
-#try hp*wt with vs and am
-m11 = lm(mpg~vs+am+wt*hp, data = cars, na.action = na.exclude)
-summary(m11)
+AIC(m4)
 
 
 #test models
-AIC(m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11)
+AIC(m1,m2,m3,m4)
 
-#best model is m10
+#best model is m9
 
-m = m10
+m = m9
 summary(m)
 
 
